ff5c419489449ef172484ba6cc04df1453b8656b
skomaromi <skomaromi@etfos.hr>
2018-05-11 01:08:21 +0200

    Fix problem with loading a captured image from a notification

--- app/src/main/java/com/github/skomaromi/whereami/MainActivity.java
+++ app/src/main/java/com/github/skomaromi/whereami/MainActivity.java
@@ -9,7 +9,6 @@ import android.app.PendingIntent;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
 import android.location.Address;
 import android.location.Criteria;
 import android.location.Geocoder;
@@ -28,7 +27,6 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -42,7 +40,6 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -349,15 +346,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
             catch (Exception e) { e.printStackTrace();}
 
-
             Uri photoURI = FileProvider.getUriForFile(this, "com.github.skomaromi.whereami.fileprovider", photoFile);
             takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
             startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
 
-
             Intent intent = new Intent(Intent.ACTION_VIEW);
-            intent.setData(photoURI);
-            intent.setType("image/*");
+            intent.setDataAndType(Uri.fromFile(photoFile), "image/*");
 
             PendingIntent notificationIntent = PendingIntent.getActivity(this, 0, intent, 0);
 
@@ -371,7 +365,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             NotificationManager notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
 
             notificationManager.notify(0, notification);
-
         }
     }
 }
